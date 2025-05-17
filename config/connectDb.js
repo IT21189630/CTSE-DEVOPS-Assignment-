31ad7224ddb558e5ea8fe40c24eb58a8d4859801
DileepLakshan141 <dileeplakshan212@gmail.com>
2025-05-17 19:19:32 +0530

    conn file updated

--- config/connectDb.js
+++ config/connectDb.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const uri =
-    "mongodb+srv://maleeshaweerasinghe27233:CSTE_2025@csteassignment.qzo29xs.mongodb.net/?retryWrites=true&w=majority&appName=CSTEassignment";
+  const uri = process.env.MONGO_DB_URI;
   if (!uri)
     throw new Error("❌ MONGO_DB_URI is not defined in environment variables!");
 
@@ -10,8 +9,8 @@ const connectDB = async () => {
     const conObj = await mongoose.connect(uri);
     console.log(`✅ Connected to the host: ${conObj.connection.host}`);
   } catch (error) {
-    console.error("❌ MongoDB Connection Error:", error.message);
-    process.exit(1);
+    console.error("❌ Error connecting to MongoDB:", error);
+    throw error;
   }
 };
 
